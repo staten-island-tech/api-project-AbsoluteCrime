d727e097cb28c31c74d9845cb18f1c0c499f294a
AbsoluteCrime <52426993+AbsoluteCrime@users.noreply.github.com>
2023-03-20 09:26:27 -0400

    search

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,26 +1,30 @@
 const randURL = "https://api.scryfall.com/cards/random";
-const searchURL = "https://api.scryfall.com/cards/search";
 const searchTerm = document.getElementById("term");
 
 async function randomCard(randURL) {
   try {
     const response = await fetch(randURL);
-    const random = await response.json();//makes the data into JSON object we can use
-    console.log(random);
-    document.getElementById("api-response").src = random.image_uris.png;
-    document.getElementById("api-response").alt = random.name;
+    const randomData = await response.json(); //makes the data into JSON object we can use
+    console.log(randomData);
+    document.getElementById("api-random-response").src =
+      randomData.image_uris.png;
+    document.getElementById("api-random-response").alt = randomData.name;
   } catch (error) {
     console.log(error);
   }
 }
 randomCard(randURL);
-async function searchData(URL) {
+async function searchCard() {
   try {
-    const searchURL
-    const searchResponse = await fetch(URL);
-
-
+    var term = document.getElementById("search").value;
+    const searchURL = `https://api.scryfall.com/cards/named?fuzzy=${term}`;
+    const response = await fetch(searchURL);
+    const searchData = await response.json();
+    console.log(searchData);
+    document.getElementById("api-search-response").src =
+      searchData.image_uris.png;
+    document.getElementById("api-search-response").alt = searchData.name;
   } catch (error) {
-
+    console.log(error);
   }
 }
