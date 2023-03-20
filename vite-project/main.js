3a6c1ff0d4c4f312386d5fbfd93e51659eb4af52
AbsoluteCrime <52426993+AbsoluteCrime@users.noreply.github.com>
2023-03-20 04:25:45 -0400

    das

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,19 +1,25 @@
-const URL = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/archmage";
+const randURL = "https://api.scryfall.com/cards/random";
+const searchURL = "https://api.scryfall.com/cards/search"
 
-async function getData(URL) {
+async function randomCard(randURL) {
   try {
-    const response = await fetch(URL, {
-      headers: {
-        'X-RapidAPI-Key': '7e1a2580c7msh192b4f51ce7e606p1b8db6jsn4b16a735f245',
-        'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
-      }
-    }
-    );
-    const data = await response.json();//makes the data into JSON object we can use
-    console.log(data);
-    document.getElementById("api-response").textContent = data.classes[0];
+    const response = await fetch(randURL);
+    const random = await response.json();//makes the data into JSON object we can use
+    console.log(random);
+    document.getElementById("api-response").src = random.image_uris.png;
+    document.getElementById("api-response").alt = random.name;
   } catch (error) {
     console.log(error);
   }
 }
-getData(URL);
+randomCard(randURL);
+async function searchData(URL) {
+  try {
+    const searchURL
+    const searchResponse = await fetch(URL);
+
+
+  } catch (error) {
+
+  }
+}
